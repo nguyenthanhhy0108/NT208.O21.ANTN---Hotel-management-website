146f818069cf2be4fa58a7b7e28386434f28d6a7
NhutPQ <22521061@gm.uit.edu.vn>
2024-04-30 20:00:46 +0700

    fix save booking

--- src/main/java/com/example/hotel_management/Controller/BookingController.java
+++ src/main/java/com/example/hotel_management/Controller/BookingController.java
@@ -50,7 +50,6 @@ public class BookingController {
     }
 
     @PostMapping("/booking")
-<<<<<<< HEAD
     public  String requestBooking(HttpServletRequest request,
                                Model model) throws ParseException {
         HttpSession session = request.getSession();
@@ -85,18 +84,11 @@ public class BookingController {
             model.addAttribute("hotelID", hotelID);
             return "book_now";
         }
-=======
-    public  String saveBooking(HttpServletRequest request,
-                               HttpServletResponse response,
-                               Model model){
-//        this.bookingServices.save(theBooking);
-        return "first-page";
->>>>>>> 99221d26a2961235ed8d6b4d5a5fc6cad112c697
     }
 
-    @PutMapping("/deleteBooking")
-    public String deleteBooking(HttpServletRequest request,
-                                Model model){
-
-    }
+//    @PutMapping("/deleteBooking")
+//    public String deleteBooking(HttpServletRequest request,
+//                                Model model){
+//
+//    }
 }
