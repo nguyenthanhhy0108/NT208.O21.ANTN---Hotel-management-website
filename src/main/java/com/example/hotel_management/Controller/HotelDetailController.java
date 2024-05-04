b8aa2751accfc99dd3cccefc748a4fb626bf63c2
Duy Thien <duythien201204@gmail.com>
2024-05-04 14:33:00 +0700

    create & update hotel & room

--- src/main/java/com/example/hotel_management/Controller/HotelDetailController.java
+++ src/main/java/com/example/hotel_management/Controller/HotelDetailController.java
@@ -1,9 +1,11 @@
 package com.example.hotel_management.Controller;
 
 import com.example.hotel_management.Model.DataDTO.RoomDTO;
+import com.example.hotel_management.Model.Hotel;
 import com.example.hotel_management.Model.HotelDetails;
 import com.example.hotel_management.Model.Room;
 import com.example.hotel_management.Service.HotelDetailsServices;
+import com.example.hotel_management.Service.HotelServices;
 import com.example.hotel_management.Service.RoomServices;
 import com.example.hotel_management.Service.UserServices;
 import jakarta.servlet.http.HttpServletRequest;
@@ -14,12 +16,11 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 @Controller
 public class HotelDetailController {
@@ -27,14 +28,17 @@ public class HotelDetailController {
     private final HotelDetailsServices hotelDetailsServices;
     private final UserServices userServices;
     private final RoomServices roomServices;
+    private final HotelServices hotelServices;
 
     @Autowired
     HotelDetailController(HotelDetailsServices hotelDetailsServices,
                           UserServices userServices,
-                          RoomServices roomServices) {
+                          RoomServices roomServices,
+                          HotelServices hotelServices) {
         this.hotelDetailsServices = hotelDetailsServices;
         this.userServices = userServices;
         this.roomServices = roomServices;
+        this.hotelServices = hotelServices;
     }
 
     @GetMapping("/hotel-detail")
@@ -44,6 +48,16 @@ public class HotelDetailController {
         if(id == null || id.isEmpty()) {
             return "first_page";
         }
+
+        List<Hotel> hotel = hotelServices.findByHotelID(id);
+        if (hotel.isEmpty()) {
+            return "redirect:/hotel-detail";
+        }
+        model.addAttribute("hotel", hotel.get(0));
+
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        Boolean isOwner = Objects.equals(hotel.get(0).getOwnerUsername(), auth.getName());
+        model.addAttribute("isOwner", isOwner);
 
         HotelDetails hotelDetails = hotelDetailsServices.findById(id);
         model.addAttribute("hotel_detail", hotelDetails);
@@ -83,4 +97,52 @@ public class HotelDetailController {
 
         return ResponseEntity.ok(this.roomServices.toDTO(availableRooms));
     }
+
+    @GetMapping("/hotel-register")
+    public String createHotel(Model model) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if(authentication.getName().equals("anonymousUser")){
+            return "hotel-details";
+        }
+
+        Hotel hotel = new Hotel(authentication.getName());
+        HotelDetails hotelDetails = new HotelDetails();
+
+        model.addAttribute("hotel", hotel);
+        model.addAttribute("hotel_detail", hotelDetails);
+
+        return "create_hotel_form";
+    }
+
+    @GetMapping("/update-hotel")
+    public String updateHotel(@RequestParam("hotel_id") String hotel_id, Model model) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if(authentication.getName().equals("anonymousUser")){
+            return "hotel-details";
+        }
+
+        List<Hotel> hotel = hotelServices.findByHotelID(hotel_id);
+        if (hotel.isEmpty() || hotel.get(0).getOwnerUsername() != authentication.getName()) {
+            return "redirect:/hotel-detail";
+        }
+        HotelDetails hotelDetails = hotelDetailsServices.findById(hotel_id);
+
+        model.addAttribute("hotel", hotel);
+        model.addAttribute("hotel_detail", hotelDetails);
+
+        return "create_hotel_form";
+    }
+
+
+    @PostMapping("/add-hotel")
+    public String addHotel(@ModelAttribute("hotel") Hotel hotel,
+                           @ModelAttribute("hotel_detail") HotelDetails hotelDetails){
+
+        hotelDetails.setHotelID(hotel.getHotelID());
+        hotelServices.saveHotel(hotel);
+        hotelDetailsServices.save(hotelDetails);
+
+        return "redirect:/hotel-detail";
+    }
+
 }
