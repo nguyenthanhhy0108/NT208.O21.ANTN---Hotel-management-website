8a5358b270ccfd103526a39dd9a23dc9990a8333
Duy Thien <duythien201204@gmail.com>
2024-05-06 13:27:50 +0700

    fix room id and hotel price

--- src/main/java/com/example/hotel_management/Model/Room.java
+++ src/main/java/com/example/hotel_management/Model/Room.java
@@ -10,6 +10,8 @@ import java.util.List;
 @Data
 @Entity
 public class Room {
+    public static int count = 50;
+
     @Id
     @Column(name = "room_id")
     @Size(min = 1, max = 50, message = "Room ID must be between 1 and 50 characters")
@@ -47,18 +49,21 @@ public class Room {
     private List<Booking> bookings;
 
 
-    public Room(String roomID, String hotelID, int numPeople, float price, int bookedGuests, float starRating, Hotel hotel) {
-        this.roomID = roomID;
+    public Room(String hotelID, int numPeople, float price, int bookedGuests, float starRating, Hotel hotel) {
         this.hotelID = hotelID;
         this.numPeople = numPeople;
         this.price = price;
         this.bookedGuests = bookedGuests;
         this.starRating = starRating;
         this.hotel = hotel;
+        count++;
+        this.roomID = String.valueOf(count);
     }
 
     public Room(){
         this.bookedGuests = 0;
         this.starRating=0;
+        count++;
+        this.roomID = String.valueOf(count);
     }
 }
