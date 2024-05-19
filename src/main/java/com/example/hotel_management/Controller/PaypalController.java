c605d5b0cae8145b4b9d1f9b303a0abdebfa9a98
Nguyen Thanh Hy <job.bkth@gmail.com>
2024-05-19 16:49:32 +0700

    fix pay

--- src/main/java/com/example/hotel_management/Controller/PaypalController.java
+++ src/main/java/com/example/hotel_management/Controller/PaypalController.java
@@ -36,8 +36,8 @@ public class PaypalController {
     public RedirectView createPayment(HttpServletRequest request) {
         try {
             String baseUrl = String.format("%s://%s", request.getScheme(), request.getServerName());
-            String cancelUrl = "http://20.2.218.245/payment/cancel";
-            String successUrl = "http://20.2.218.245/payment/success";
+            String cancelUrl = "http://ho12.site:8080//payment/cancel";
+            String successUrl = "http://ho12.site:8080//payment/success";
             Payment payment = paypalService.createPayment(
                     Double.parseDouble(request.getParameter("amount")),
                     "USD",
